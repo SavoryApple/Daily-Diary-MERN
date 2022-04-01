c4b45f045e716b04338afad6daceefa59b193ab9
Seann Avery <SeannAvery@gmail.com>
2022-04-01 10:52:22 -0700

    removed unused components

--- src/components/mountain.jsx
+++ src/components/mountain.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './css/Mountain.css';
 
-const Mountain = (props)=>{
-    return(
+const Mountain = (props) => {
+    return (
         <div>
-            {/* <img className="sunrise" src={require("../images/sunrise.png")}/> */}
-            <img alt='mountain' className="mountain" src={require("../images/mountain.png")}/>
+            <img alt='mountain' className="mountain" src={require("../images/mountain.png")} />
         </div>
     )
 }
